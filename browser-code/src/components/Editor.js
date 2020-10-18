bde76ec2c6ee193b483a5c1b6a85d4bc5c3e55ad
matthajec <66030008+matthajec@users.noreply.github.com>
2020-10-17 22:43:34 -0400

    started switching to monaco editor

--- browser-code/src/components/Editor.js
+++ browser-code/src/components/Editor.js
@@ -1,34 +1,29 @@
 import React from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import { Controlled as ControlledEditor } from 'react-codemirror2'
+
+import MonacoEditor from 'react-monaco-editor'
 
 export default function Editor(props) {
+
     const {
         language,
         value,
         onChange
     } = props
 
-    function handleChange(editor, data, value) {
-        onChange(value)
-    }
-
     return (
         <div className="editor">
-            <ControlledEditor 
-                onBeforeChange={handleChange}
-                value={value}
+            <MonacoEditor 
+                width="100vw"
+                height="35vh"
+                theme="vs-dark"
                 options={{
-                    mode: language,
-                    theme: 'material',
-                    lineNumbers: true,
-                    lineWrapping: true
+                    autoClosingBrackets: true
                 }}
+                language={language}
+                value={value}
+                onChange={onChange}
             />
+
         </div>
     )
 }
